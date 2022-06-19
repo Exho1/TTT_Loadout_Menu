47d602886dd4dfc9433fe02c6e742eb4dcd6e43c
RIPD <fabian787878@gmail.com>
2022-06-19 21:03:26 +0200

    Added weapons to loadout_config

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ TTT_Loadout_Menu
 Created by Exho on August 15th, 2014 and initially coded in 2 hours. Its my clone of a Coderhire addon that I saw. 
 
 Almost entirely rewritten on 7/12/15 to just work better. Added scrollers for more weapons as well as reducing boilerplate code on the client and improving the serverside code as well.
+
+
+Modified loadout_config.lua to also Show Weapons from the follwoing collections:
+[TTT Rifle Pack](https://steamcommunity.com/sharedfiles/filedetails/?id=307400737)
+[TTT Assault Pack](https://steamcommunity.com/sharedfiles/filedetails/?id=307400139)
+[TTT Assault Pack 2](https://steamcommunity.com/sharedfiles/filedetails/?id=316433211)#
+[TTT Rifle Pack](https://steamcommunity.com/sharedfiles/filedetails/?id=307400737)
+[TTT Pistol Pack](https://steamcommunity.com/sharedfiles/filedetails/?id=307401169)
+
--- lua/autorun/loadout_config.lua
+++ lua/autorun/loadout_config.lua
@@ -20,6 +20,14 @@ loadout.primary = {
 	["HUGE"] = "weapon_zm_sledge",
 	["Mac10"] = "weapon_zm_mac10",
 	["Scout"] = "weapon_zm_rifle",
+	["MR-CA1"] = "weapon_ap_mrca1", --TTT Assault Pack 2 id=316433211
+	["TEC-9"] = "weapon_ap_tec9",
+	["Vector"] = "weapon_ap_vector",
+	["Honey Badger"] = "weapon_ap_hbadger", --TTT Assault Pack id=307400139
+	["PP19"] = "weapon_ap_pp19",
+	["Double Barrel"] = "weapon_sp_dbarrel", --TTT Shotgun Pack id=307345118
+	["Striker 12"] = "weapon_sp_striker",
+	["Winchester"] = "weapon_sp_winchester",
 }
 
 -- Secondary weapons. Format: Name = Class
@@ -27,6 +35,9 @@ loadout.secondary = {
 	["Five Seven"] = "weapon_zm_pistol",
 	["Glock"] = "weapon_ttt_glock",
 	["Deagle"] = "weapon_zm_revolver",
+	["Raging Bull"] = "weapon_pp_rbull", --TTT Pistol Pack id=307401169
+	["Remington"] = "weapon_pp_remington",
+	["Pocket Rifle"] = "weapon_rp_pocket", --TTT Rifle Pack id=307400737
 }
 
 -- Extra weapons. Format: Name = Class
@@ -46,6 +57,18 @@ loadout.icons = {
 	["weapon_zm_pistol"] = "vgui/ttt/icon_pistol",
 	["weapon_ttt_glock"] = "vgui/ttt/icon_glock",
 	["weapon_zm_revolver"] = "vgui/ttt/icon_deagle",
+	["weapon_ap_mrca1"] = "vgui/ttt/lykrast/icon_ap_mrca1", --TTT Assault Pack 2 id=316433211
+	["weapon_ap_tec9"] = "vgui/ttt/lykrast/icon_ap_tec9",
+	["weapon_ap_vector"] = "vgui/ttt/lykrast/icon_ap_vector",
+	["weapon_ap_hbadger"] = "vgui/ttt/lykrast/icon_ap_hbadger", --TTT Assault Pack id=307400139
+	["weapon_ap_pp19"] = "vgui/ttt/lykrast/icon_ap_pp19.vtf",
+	["weapon_pp_rbull"] = "vgui/ttt/lykrast/icon_pp_rbull", --TTT Pistol Pack id=307401169
+	["weapon_pp_remington"] = "vgui/ttt/lykrast/icon_pp_remington",
+	["weapon_rp_pocket"] = "vgui/ttt/lykrast/icon_rp_pocket.vmt", --TTT Rifle Pack id=307400737
+	["weapon_sp_dbarrel"] = "vgui/ttt/lykrast/icon_sp_dbarrel", --TTT Shotgun Pack id=307345118
+	["weapon_sp_striker"] = "vgui/ttt/lykrast/icon_sp_striker",
+	["weapon_sp_winchester"] = "vgui/ttt/lykrast/icon_sp_winchester",
+
 }
 
 local plymeta = FindMetaTable( "Player" )
